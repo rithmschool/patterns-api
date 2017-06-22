ec37d704fcd111b21c062d9da1a697e1c9d5ba35
Tobi Ogunnaike <oluwatobi.ogunnaike@gmail.com>
2017-06-22 14:37:33 -0700

    writing route tests, cleaning up routes

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ var app = express();
 var methodOverride = require("method-override");
 var morgan = require("morgan");
 var bodyParser = require("body-parser");
-var jwt = require('jsonwebtoken');
 var passport = require("passport");
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var db = require("./models");
 var cors = require("cors");
-var axios = require('axios');
-var qs = require("qs");
+var authRoutes = require('./routes/auth');
+var assetRoutes = require('./routes/assets');
 
 if (process.env.NODE_ENV !== 'production') {
   require("dotenv").config();
@@ -35,60 +33,11 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-app.get('/auth/google',
-  passport.authenticate('google', {
-    scope: [
-      'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/userinfo.profile', 
-      'https://www.googleapis.com/auth/userinfo.email'
-    ]
-  })
-);
-
-app.post('/auth/google/callback',
-  function(request, response) {
-    axios({
-      method: 'post',
-      url: 'https://www.googleapis.com/oauth2/v4/token',
-      data: qs.stringify({
-        code: request.body.code,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: process.env.CALLBACK_URL,
-        grant_type: 'authorization_code'
-      }),
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded' 
-      }
-    })
-    .then(function(res){
-      return axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${res.data.access_token}`);
-    })
-    .then(function(res){
-      db.User.findOrCreate({ 
-        googleId: res.data.id, 
-        firstName: res.data.given_name, 
-        lastName: res.data.family_name,
-        email:res.data.email 
-      }, 
-      function (err, user) {
-        if (err) { return done(err); }
-        const payload = {
-          googleId: user.googleId,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email
-        }
-        const token = jwt.sign(payload, process.env.SECRET_KEY);
-        response.status(200).send(token);
-      });
-    })
-    .catch(function(error) {
-      response.status(500).send(error);
-    });
-  }
-);
+app.use('/auth', authRoutes);
+app.use('/types/:id/assets', assetRoutes);
 
 app.listen(3001, function() {
   console.log("Server is listening on port 3001");
 });
+
+module.exports = app;
