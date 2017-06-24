0471270a0700c29d08737ca1f161aa4b0de4d2aa
Shriya K Nevatia <sneva831@gmail.com>
2017-06-23 17:17:38 -0700

    ok really updating routing file structure

--- test/routes/assetSpec.js
+++ test/routes/assetSpec.js
@@ -8,27 +8,26 @@ const request = require('supertest');
 const jwt = require('jsonwebtoken');
 const expect = require('chai').expect;
 
-describe('GET /types/:id/assets', function() {
-  let type = null;
+describe('GET /assets/:a_id/childassets', function() {
+  let asset = null;
+  let child = null;
   before(function(done) {
-    db.Type.create({ 
-      isAgent: false, 
-      name:'Brand'
+    db.Asset.create({
+      name:'Facebook',
+      url:'https://www.facebook.com/',
+      logo:'https://facebookbrand.com/wp-content/themes/fb-branding/prj-fb-branding/assets/images/fb-art.png'
     })
-    .then(function(newType) {
-      type = newType;
-      const asset = new db.Asset({
-        name:'Google',
-        url:'https://www.google.com/',
-        logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2000px-Google_2015_logo.svg.png'
+    .then(function(newAsset) {
+      asset = newAsset;
+      child = new db.Asset({
+        name:'Funding'
       })
-      asset.typeId = type.id;
+      return child.save()
+    })
+    .then(function(child) {
+      asset.assets.push(child._id);
       return asset.save()
     })
-    .then(function(asset) {
-      type.assets.push(asset.id);
-      return type.save()
-    })
     .then(function() {
       done();
     })
@@ -37,100 +36,27 @@ describe('GET /types/:id/assets', function() {
     });
   });
 
-  it('responds with an array of assets if token is valid', function(done) {
+  it('adds a child asset to asset list if token is valid', function(done) {
     const token = login(testingData);
     request(app)
-      .get(`/types/${type.id}/assets`)
+      .get(`/assets/${asset.id}/childassets`)
       .set('authorization', 'Bearer: ' + token)
       .expect(200)
       .expect(function(res) {
-        expect(res.body.isAgent).to.be.false;
-        expect(res.body.name).to.equal('Brand');
+        expect(res.body.name).to.equal('Facebook');
+        expect(res.body.url).to.equal('https://www.facebook.com/');
+        expect(res.body.logo).to.equal('https://facebookbrand.com/wp-content/themes/fb-branding/prj-fb-branding/assets/images/fb-art.png');
         expect(res.body.assets.length).to.equal(1);
-        expect(res.body.assets[0].name).to.equal('Google');
-        expect(res.body.assets[0].url).to.equal('https://www.google.com/');
-        expect(res.body.assets[0].logo).to.equal('https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2000px-Google_2015_logo.svg.png');
+        expect(res.body.assets[0].name).to.equal('Funding');
       })
-      .end(done);
-  }); 
-
-  it('should be invalid if token is invalid', function(done) {
-    request(app)
-      .get(`/types/${type.id}/assets`)
-      .set('authorization', 'Bearer: ' + jwt.sign(testingData, 'wrong key'))
-      .expect(401, {
-        message: "Invalid user."
-      }, done);
-  });
-
-  it('it should be invalid if there is no token', function(done) {
-    request(app)
-      .get(`/types/${type.id}/assets`)
-      .expect(401, {
-        message: "You must be logged in to continue."
-      }, done);
+    .end(done);
   });
 
   after(function(done) {
-    db.Type.remove({})
-    .then(function() {
-      return db.Asset.remove({})
-    })
+    db.Asset.remove({})
     .then(function() {
       done();
     });
   });
-});
 
-describe('POST /types/:id/assets', function() {
-  let type = null;
-  before(function(done) {
-    db.Type.create({ 
-      isAgent: false, 
-      name:'Conglomerate'
-    })
-    .then(function(newType) {
-      type = newType;
-      done();
-    })
-    .catch(function(error){
-      console.log(error);
-    });
-  })
-
-  it('creates a new asset of the given type if token is valid', function(done) {
-    const token = login(testingData);
-    request(app)
-      .post(`/types/${type.id}/assets`)
-      .send({
-        name:'Amazon',
-        url:'https://www.amazon.com/',
-        logo:'http://static1.businessinsider.com/image/539f3ffbecad044276726c01-960/amazon-com-logo.jpg'
-      })
-      .set('authorization', 'Bearer: ' + token)
-      .expect(200)
-      .expect(function(res) {
-        expect(res.body.name).to.equal('Amazon');
-        expect(res.body.url).to.equal('https://www.amazon.com/');
-        expect(res.body.logo).to.equal('http://static1.businessinsider.com/image/539f3ffbecad044276726c01-960/amazon-com-logo.jpg');
-        expect(res.body.id).to.not.be.null;
-      })
-      .end(done);
-    });
-
-    it('it should be invalid if there is no token', function(done) {
-      request(app)
-        .post(`/types/${type.id}/assets`)
-        .send({random:"data"})
-        .expect(401, {
-          message: "You must be logged in to continue."
-        }, done);
-    });
-
-  after(function(done) {
-    db.Type.remove({})
-    .then(function() {
-      done();
-    });
-  });
 });
