9dc9f4992ee33af8ce062a29e7d56372e1f1f02e
Matt Lane <lane.matthew@gmail.com>
2017-06-28 12:32:36 -0700

    adding config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    mongooseDebug: true,
+    mongoUri: 'mongodb://localhost/patterns',
+    port: 3001,
+    useEnv: true,
+    useMorgan: true,
+  },
+  test: {
+    mongooseDebug: false,
+    mongoUri: 'mongodb://localhost/patterns-test',
+    port: 3002,
+    useEnv: true,
+    useMorgan: false
+  },
+  production: {
+    mongooseDebug: false,
+    mongoUri: process.env.MONGODB_URI,
+    port: process.env.PORT,
+    useEnv: false,
+    useMorgan: false
+  }
+}
+
+module.exports = config[env];
