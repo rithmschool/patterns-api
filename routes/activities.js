2beda8c03aa0785fc9539262643c1a7969c1ff9f
Matt Lane <lane.matthew@gmail.com>
2017-06-30 09:48:57 -0700

    updating tests for activitySpec GET

--- routes/activities.js
+++ routes/activities.js
@@ -5,12 +5,9 @@ const db = require("../models");
 const ensureCorrectUser = require('./helpers').ensureCorrectUser;
 
 router.get('/', function(req, res) {
-  db.User.findById(req.params.u_id).populate('activities')
-    .then(function(user){
-      db.Activity.find(user.activities).populate('stages')
-      .then(function(activities){
-        res.send(activities);
-      })
+    db.Activity.find({createdBy: req.params.u_id}).populate('stages')
+    .then(function(activities){
+      res.send(activities);
     })
     .catch(function(err){
       res.status(500).send(err);
