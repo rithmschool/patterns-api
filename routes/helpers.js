dddf0a9cfd73f0a3ee06cd6e63c146b17853a495
Katie Krieger <katielkrieger@gmail.com>
2017-06-30 13:46:50 -0700

    Refactored ensureCorrectUser helper function

--- routes/helpers.js
+++ routes/helpers.js
@@ -22,8 +22,15 @@ function loginRequired(req, res, next){
 
 function ensureCorrectUser(req, res, next){
   const authHeader = req.headers['authorization'];
+  let param = null;
   if(authHeader) {
     let token = authHeader.split(" ")[1];
+    let mappedModel = {
+      c_id: db.Asset,
+      a_id: db.Asset,
+      t_id: db.Type
+    }
+
     jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
       if(req.params.u_id) {
         if(!err && decoded.mongoId === req.params.u_id) {
@@ -33,38 +40,17 @@ function ensureCorrectUser(req, res, next){
             message: "Unauthorized"
           });
         }
-      } else if(req.params.c_id) {
-        db.Asset.findById(req.params.c_id)
-        .then(function(foundAsset) {
-          if(!err && decoded.mongoId === foundAsset.createdBy.toString()) {
-            next();
-          } else {
-            throw "Error"
-          }
-        })
-        .catch(function() {
-          res.status(401).send({
-            message: "Unauthorized"
-          })
-        });
-      } else if (req.params.a_id) {
-        db.Asset.findById(req.params.a_id)
-        .then(function(foundAsset) {
-          if(!err && decoded.mongoId === foundAsset.createdBy.toString()) {
-            next();
-          } else {
-            throw "Error"
-          }
-        })
-        .catch(function() {
-          res.status(401).send({
-            message: "Unauthorized"
-          })
-        });
-      } else if (req.params.t_id) {
-        db.Type.findById(req.params.t_id)
-        .then(function(foundType) {
-          if(!err && decoded.mongoId === foundType.createdBy.toString()) {
+      } else {
+        if (req.params.c_id) {
+          param = "c_id";
+        } else if (req.params.a_id) {
+          param = "a_id";
+        } else if (req.params.t_id) {
+          param = "t_id";
+        }
+        mappedModel[param].findById(req.params[param])
+        .then(function(foundItem) {
+          if(!err && decoded.mongoId === foundItem.createdBy.toString()) {
             next();
           } else {
             throw "Error"
